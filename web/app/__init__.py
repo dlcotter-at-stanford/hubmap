cdb5d29070aea9995d1cadef1eb6dfdd972294e3
Daniel Cotter <daniel.cotter@stanford.edu>
2020-06-09 10:28:12 -0700

    Added option to run Flask locally for debugging (helpful since it reloads code automatically when it's changed)

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -12,4 +12,8 @@ if 'FLASK_ENV' in os.environ:
 
 from app import routes
 
-waitress.serve(app, host='0.0.0.0', port=5000)
+if 'FLASK_ENV' in os.environ:
+    if os.environ['FLASK_ENV'] == 'dev':
+      app.run()
+    if os.environ['FLASK_ENV'] == 'prd':
+      waitress.serve(app, host='0.0.0.0', port=5000)
