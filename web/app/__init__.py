b0ca4a37805929be157f2bd3b4a56ff847e63c65
Daniel Cotter <daniel.cotter@stanford.edu>
2020-06-09 11:14:30 -0700

    Changes to run script and app init to make debugging and deployment more flexible

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -4,16 +4,13 @@ import flask
 
 app = flask.Flask(__name__)
 
-if 'FLASK_ENV' in os.environ:
-    if os.environ['FLASK_ENV'] == 'dev':
-        app.config.from_envvar('FLASK_DEV_CONFIG')
-    if os.environ['FLASK_ENV'] == 'prd':
-        app.config.from_envvar('FLASK_PRD_CONFIG')
-
 from app import routes
 
+if 'FLASK_CONFIG' in os.environ:
+  app.config.from_envvar('FLASK_CONFIG')
+
 if 'FLASK_ENV' in os.environ:
-    if os.environ['FLASK_ENV'] == 'dev':
-      app.run()
-    if os.environ['FLASK_ENV'] == 'prd':
-      waitress.serve(app, host='0.0.0.0', port=5000)
+  if os.environ['FLASK_ENV'] == 'dev':
+    app.run()
+  if os.environ['FLASK_ENV'] == 'prd':
+    waitress.serve(app, host='0.0.0.0', port=5000)
--- web/run-app.sh
+++ web/run-app.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
-export FLASK_PRD_CONFIG=../prd.cfg
-export FLASK_DEV_CONFIG=../dev.cfg
+
 export FLASK_ENV=dev
 
-python app.py &
+# I had been using dev.cfg and prd.cfg to control whether the app ran in debug
+# mode, but the app doesn't seem to pick up the values from there for some
+# reason, so I've moved that part of the configuration into the run script. This
+# also lets me control whether the app runs in the foreground (for debugging)
+# or the background (for production).
+
+if [[ $FLASK_ENV=dev ]]
+then
+  export FLASK_CONFIG=../dev.cfg
+  export FLASK_DEBUG=True
+  python app.py
+fi
+
+if [[ $FLASK_ENV=prd ]]
+then
+  export FLASK_CONFIG=../prd.cfg
+  export FLASK_DEBUG=False
+  python app.py &
+fi
+
