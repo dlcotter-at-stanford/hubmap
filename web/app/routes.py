a2cda21b7ae7e383ec74be5f5844ba46f96d5b47
Daniel Cotter <daniel.cotter@stanford.edu>
2020-07-28 10:59:00 -0700

    Moved map page to index and added error-handling logic

--- web/app/routes.py
+++ web/app/routes.py
@@ -1,21 +1,19 @@
 from app import app
 import database
 import flask
+import pdb
 
 @app.route('/')
-def index():
-  return flask.render_template('index.html'
-                              ,title='Colon map visualization'
-                              ,subjects=database.Database(app.config).get_subjects_with_mapped_samples())
-
-@app.route('/map/<subject>')
-def map(subject):
+@app.route('/<subject>')
+def map(subject=None):
   db = database.Database(app.config) 
 
   # If there are no subjects found or the requested subject is not found, redirect:
   subjects = db.get_subjects_with_mapped_samples()
   subject_ids = db.get_results_by_column_name(subjects, 'subject')
-  if subjects is None or subject not in subject_ids:
+
+  # Case: Requested subject not in list of subjects with mapped samples
+  if subject and subject not in subject_ids:
     return flask.redirect('/')
 
   # Get the data sets to be displayed as tables under the visualization.
@@ -24,11 +22,11 @@ def map(subject):
   datasets = { 'clinical' : db.get_samples_with_coordinates(subject)
               ,'pathology': db.get_pathology_by_subject(subject)
               ,**           db.get_all_metadata(subject) }
-
+  
   # Prune empty data sets (note: list(...) prevents error from modifying dict
   # while iterating over it)
   for ds in list(datasets):
     if not datasets[ds]['data']:
       del datasets[ds]
 
-  return flask.render_template('map.html', title='Samples', subject=subject, tables=datasets, other_subjects=subjects)
+  return flask.render_template('index.html', title='Samples', subject=subject, tables=datasets, other_subjects=subjects)
