6d00d70332ab8ad8e3321e4bd2987407d02d9cde
Daniel Cotter <daniel.cotter@stanford.edu>
2020-07-29 21:48:39 -0700

    More deployment stuff from the local repo

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-#!/bin/zsh
+#!/bin/zsh --verbose
 
 # Pull down changes from origin GitHub repo to GCP VM
-if gcloud compute ssh "postgresql-1-vm" --zone "us-west1-a" --command "cd hubmap; git pull"; then : nothing; fi
+if gcloud compute ssh "postgresql-1-vm" --zone "us-west1-a" --command "cd biolab; git pull"; then : nothing; fi
 
 # Securely copy remaining source data to VM using gcloud tools
-if gcloud compute scp --recurse ~/Documents/work/HuBMAP/dev/data-portal/db/data "postgresql-1-vm:~/hubmap/db" --zone "us-west1-a"; then : nothing; fi
+if gcloud compute scp --recurse ~/Documents/work/HuBMAP/dev/data-portal/db/data "postgresql-1-vm:~/biolab/db" --zone "us-west1-a"; then : nothing; fi
 
 # Not copying the config file with environment-specific and sensitive information (copy manually instead)
 
 # Rebuild database from scratch on VM
-if gcloud compute ssh "postgresql-1-vm" --zone "us-west1-a" --command "cd hubmap/db/scripts; ./build-database.sh"; then : nothing; fi
+if gcloud compute ssh "postgresql-1-vm" --zone "us-west1-a" --command "cd biolab; source config.sh; cd db/scripts; ./build-database.sh; cd ../../web; ./run-app.sh"; then : nothing; fi
